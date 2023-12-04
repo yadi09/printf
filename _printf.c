bb4959e70f5633c05a352b06bafea5725ba30cda
yadamzer <yadamzerterefe09@gmail.com>
2023-12-03 17:02:33 -0800

    Printf

--- _printf.c
+++ _printf.c
@@ -2,49 +2,50 @@
 
 /**
  * _printf -  function that produces output according to a format.
- * @format: character string. 
+ * @format: character string.
  * Return: length of formated output string
 */
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, length = 0;
-    int (*desire_func)(va_list);
+va_list args;
+int i = 0, length = 0;
+int (*desire_func)(va_list);
 
-    if (format == NULL)
-    return (-1);
+if (format == NULL)
+return (-1);
 
-    va_start(args, format);
-
-    while (format[i])
-    {
-        if (format[i] != '%')
-        {
-            write(1, &format[i], 1);
-            length++;
-        }
-        else
-        {
-            i++;
-            if (format[i] == '%')
-            {
-                write(1, "%", 1);
-                length++;
-            }
-            else
-            {
-                desire_func = check_specifier(&format[i]);
-                if (desire_func == NULL)
-                {
-                    _printf("%%%c", format[i]);
-                }
-                else
-                length += desire_func(args);
-            }
-        }
-        i++;
-    }
-    va_end(args);    
-    return (length);
+va_start(args, format);
+if (format[0] == '%' && !format[1])
+return (-1);
+while (format[i])
+{
+if (format[i] != '%')
+{
+write(1, &format[i], 1);
+length++;
+}
+else
+{
+i++;
+if (format[i] == '%')
+{
+write(1, "%", 1);
+length++;
+}
+else
+{
+desire_func = check_specifier(&format[i]);
+if (desire_func == NULL)
+{
+_printf("%%%c", format[i]);
+}
+else
+length += desire_func(args);
+}
+}
+i++;
+}
+va_end(args);
+return (length);
 }
