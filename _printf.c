715c9743624aeeff1fde2abe2ed3f3e5653c23d9
yadamzer <yadamzerterefe09@gmail.com>
2023-12-03 17:13:46 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -15,8 +15,7 @@ int (*desire_func)(va_list);
 if (format == NULL)
 return (-1);
 va_start(args, format);
-if (format[0] == '%' && !format[1] &&
-format[0] == '%' && format[1] == ' ' && !format[2])
+if (format[0] == '%' && !format[1])
 	return (-1);
 while (format[i])
 {
